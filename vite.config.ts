262189bea56f58b41a2ccebe7275cf5c39891ef7
Nikita Nitichevski <me@donnikitos.com>
2022-04-30 20:41:29 +0200

    added jsx configuration to vite and typescript

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,9 @@
 import { defineConfig } from 'vite';
-import path from 'path';
 
 export default defineConfig({
-	build: {
-		lib: {
-			entry: path.resolve(__dirname, 'src/lib/index.ts'),
-			fileName: 'index',
-			formats: ['umd', 'es'],
-			name: '@mutablejs/dom',
-		},
-		minify: false,
+	esbuild: {
+		jsx: 'transform',
+		jsxFactory: 'mutableElement',
+		jsxInject: "import mutableElement from '@mutablejs/dom';",
 	},
 });
